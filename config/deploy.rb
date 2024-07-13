e186e1eaafd838443b4fb5198cce8a2e3f828f75
Igor <dont@bzzr.me>
2024-07-13 15:08:23 +0100

    Minor update

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "hello-rails"
 set :repo_url, "git@github.com:i9or/hello-rails.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/deploy/#{fetch :application}"
